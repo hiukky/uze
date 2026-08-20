941be63c2928ed920750894c63081224ea3b6f2a
Romullo <developermarsh@gmail.com>
2026-08-20 13:19:11 -0300

    chore: exclude conformance build artifacts

--- tooling/conformance/target/debug/deps/uze_conformance-e99022b8354562f3.d
+++
@@ -1,5 +0,0 @@
-/home/hiukky/uze/tooling/conformance/target/debug/deps/uze_conformance-e99022b8354562f3.d: src/main.rs
-
-/home/hiukky/uze/tooling/conformance/target/debug/deps/uze_conformance-e99022b8354562f3: src/main.rs
-
-src/main.rs:
